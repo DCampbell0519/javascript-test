fdeb13e3a9a42a2dd126ea0a8e0c4e716933917a
Donovan Campbell <donovan@dlcentertainment.net>
2025-04-01 19:09:25 -0700

    who knows

---
+++ node/scratch.js
@@ -0,0 +1,24 @@
+// Thinking in javascript
+// This is a comment, specifically a single line comment
+/* 
+*
+* This is a multi-line comment
+*/
+// Rule of thumb, no comment over 80 columns.  (columns = characters)
+
+// How to print to the console, the second most important thing to learn in any language.
+console.log("Hello world!")
+
+// Variables
+// Declared, using very specific words.  
+
+// let - a mutable variable, the value can be changed.  This may be reassigned.
+
+// = is the assignment operator, this does not represent equality.
+let myName = "Donovan" // String begins with ", ', ` and contains characters.  
+
+// const - is an immutable variable, meaning it cannot be reassigned.  
+const theSky = "is blue"
+
+// var - is mutable, and is contained in the global scope.  
+//It receives the super power of hoisting, 
