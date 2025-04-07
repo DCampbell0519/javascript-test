e4340f7556ee1fa534ef19179c9af442afa77dc4
Donovan Campbell <donovan@dlcentertainment.net>
2025-04-06 18:31:29 -0700

    First problem done

---
+++ warm-ups.js
@@ -0,0 +1,10 @@
+// 1. printGreeting
+// Write a function called printGreeting with a parameter name that returns a greeting with the argument interpolated into the greeting.
+// // console.log(printGreeting("Slimer"));
+// // => Hello there, Slimer!
+
+const printGreeting = (name) => {
+    return `Hello There, ${name}!`;
+}
+console.log(printGreeting("Slimer"));
+
